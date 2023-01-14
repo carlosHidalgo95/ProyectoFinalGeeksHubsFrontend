069b73cb86ceb866f864a3ae1d6b0a4ece904c5e
carlosHidalgo95 <karucupcake@gmail.com>
2023-01-14 20:23:33 +0100

    cards de antd cambiadas por las de bootstrap y css añadido al container de booking

--- src/Components/Dishes/DishBox.jsx
+++ src/Components/Dishes/DishBox.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Card } from 'antd';
+// import { Card } from 'antd';
+import {Row,Col,Card} from 'react-bootstrap';
+import './DishBox.scss';
 
 const DishBox = ({dish}) => {
 
     return (
-        <Card key={dish.name} hoverable style={{width: 240,margin:10}}
-        cover={<img alt="dish_poster" src={dish.img} />}>
-            <h4>{dish.dish_name}</h4>
-        </Card>
+        
+        // <Card className='dishCard' key={dish.name} hoverable style={{width: 240,margin:10}}
+        // cover={<img alt="dish_poster" src={dish.img} />}>
+        //     <h4>{dish.dish_name}</h4>
+        // </Card>
+
+
+
+<Card>
+<Card.Img variant="top" src={dish.img} />
+<Card.Body>
+  <Card.Title>{dish.dish_name}</Card.Title>
+</Card.Body>
+</Card>
     );
 };
 
